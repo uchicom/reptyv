06826556c9339ed5389d062490d91b0ed4baaee9
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2024-09-20 16:25:37 +0900

    フォーマッタを追加する #33

--- src/main/java/com/uchicom/reptyv/Main.java
+++ src/main/java/com/uchicom/reptyv/Main.java
@@ -1,3 +1,4 @@
+// (C) 2024 uchicom
 package com.uchicom.reptyv;
 
 import javax.swing.JTextArea;
@@ -5,26 +6,24 @@ import javax.swing.SwingUtilities;
 
 /**
  * keyとマッピングさせたcsvを読み込んでpdfに出力する
- * 
- * @author hex
  *
+ * @author hex
  */
 public class Main {
 
-	public static void main(String[] args) {
-		// viewer起動
-		SwingUtilities.invokeLater(() -> {
-			JTextArea editorText = new JTextArea();
-			JTextArea parameterText = new JTextArea();
-			ReptyViewer viewer = new ReptyViewer(editorText, parameterText);
-			ReptyEditor reptyEditor = new ReptyEditor(viewer, editorText);
-			ParameterEditor parameterEditor = new ParameterEditor(viewer, parameterText);
-			
-			viewer.setVisible(true);
-			reptyEditor.setVisible(true);
-			parameterEditor.setVisible(true);
-		});
+  public static void main(String[] args) {
+    // viewer起動
+    SwingUtilities.invokeLater(
+        () -> {
+          JTextArea editorText = new JTextArea();
+          JTextArea parameterText = new JTextArea();
+          ReptyViewer viewer = new ReptyViewer(editorText, parameterText);
+          ReptyEditor reptyEditor = new ReptyEditor(viewer, editorText);
+          ParameterEditor parameterEditor = new ParameterEditor(viewer, parameterText);
 
-	}
-
+          viewer.setVisible(true);
+          reptyEditor.setVisible(true);
+          parameterEditor.setVisible(true);
+        });
+  }
 }
--- src/main/java/com/uchicom/reptyv/ParameterEditor.java
+++ src/main/java/com/uchicom/reptyv/ParameterEditor.java
@@ -1,29 +1,27 @@
+// (C) 2024 uchicom
 package com.uchicom.reptyv;
-
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 import com.uchicom.ui.ResumeDialog;
 import com.uchicom.ui.ResumeFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class ParameterEditor extends ResumeDialog {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+  /** */
+  private static final long serialVersionUID = 1L;
 
-	private JTextArea textArea;
+  private JTextArea textArea;
 
-	public ParameterEditor(ResumeFrame repptyViewer, JTextArea textArea) {
-		super(repptyViewer, "parameter.editor");
-		this.textArea = textArea;
-		initComponents();
-	}
+  public ParameterEditor(ResumeFrame repptyViewer, JTextArea textArea) {
+    super(repptyViewer, "parameter.editor");
+    this.textArea = textArea;
+    initComponents();
+  }
 
-	private void initComponents() {
-		setTitle("Parameter");
-		getContentPane().add(new JScrollPane(textArea));
-		pack();
-	}
+  private void initComponents() {
+    setTitle("Parameter");
+    getContentPane().add(new JScrollPane(textArea));
+    pack();
+  }
 }
